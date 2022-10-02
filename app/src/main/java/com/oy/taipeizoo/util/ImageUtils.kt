96fa633015d5eff7e11113adcfe42104c9f185d3
MarsYang <billyeric2010@gmail.com>
2022-10-02 10:30:31 +0800

    [FIX_BUG] Q:when img url string is not empty,but not show image A:url source http replace https

--- app/src/main/java/com/oy/taipeizoo/util/ImageUtils.kt
+++ app/src/main/java/com/oy/taipeizoo/util/ImageUtils.kt
@@ -3,10 +3,12 @@ package com.oy.taipeizoo.util
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.util.Log
 import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
@@ -14,14 +16,19 @@ import com.bumptech.glide.request.transition.Transition
 import com.oy.taipeizoo.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+
 const val DEFAULT_IMAGE = R.drawable.no_image
 
-@SuppressLint("UnrememberedMutableState")
+
 @ExperimentalCoroutinesApi
 @Composable
 fun loadPicture(url: String, @DrawableRes defaultImage: Int): MutableState<Bitmap?> {
 
     val bitmapState: MutableState<Bitmap?> = mutableStateOf(null)
+    val replaceString:String  = url.replace("https","http").
+                                    replace("http","https").
+                                    replace(" ","%20")
+    Log.d("loadPicture", "loadPicture: ${replaceString}")
 
     // show default image while image loads
     Glide.with(LocalContext.current)
@@ -37,19 +44,19 @@ fun loadPicture(url: String, @DrawableRes defaultImage: Int): MutableState<Bitma
             }
         })
 
-    // get network image
-    Glide.with(LocalContext.current)
-        .asBitmap()
-        .load(url)
-        .into(object : CustomTarget<Bitmap>() {
-            override fun onLoadCleared(placeholder: Drawable?) { }
-            override fun onResourceReady(
-                resource: Bitmap,
-                transition: Transition<in Bitmap>?
-            ) {
-                bitmapState.value = resource
-            }
-        })
+        // get network image
+        Glide.with(LocalContext.current)
+            .asBitmap()
+            .load(replaceString)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) { }
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap>?
+                ) {
+                    bitmapState.value = resource
+                }
+            })
 
     return bitmapState
 }
